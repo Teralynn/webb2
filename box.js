57b2edcf0d4ba1f0af671032cc6b968e041ddfd7
Teralynn <daniel.hakansson.te11c@gmail.com>
2014-03-06 10:06:05 +0100

    Version 1.1
    Updated

--- box.js
+++ box.js
@@ -1,21 +1,33 @@
-// JavaScript Document
+// imgs är min array med bilder
+// cnt hjälper till räkna ut antal bilder i arrayen
+// bildspel är knappen till vänster på sidan
+// bildspel2 är knappen till höger på sidan
+// Använder mig av (this).attr för att fadeIn rätt bild
 $(document).ready(function(){
 	
-var elems = document.getElementsByTagName( "pic" );
-// Convert the NodeList to an Array
-var arr = jQuery.makeArray( elems );
-// Use an Array method on list of dom elements
-arr.reverse();
-
-$( arr ).appendTo( document.body );
+var imgs = [								
+           'photos/glad.jpg',
+           'photos/mario.jpg',
+		   'photos/placeholder.jpg'
+		   ];
+var cnt = imgs.length;
 		
-    $('#bildspel').click(function(){	 
-		  $('imgArray[0]').fadeIn('swing');
-		  $('imgArray[1]').fadeOut('swing');
-	});
-	
-	$('#bildspel2').click(function() {
-		  $('imgArray[0]').fadeOut('swing');
-		  $('imgArray[1]').fadeIn('swing');
-	});
+		 
+		 $('#bildspel').click(function() {
+			 
+			 $('#imageSlide').fadeOut("fast", function() {
+				 
+         	  	  $(this).attr('src', imgs[(imgs.length++) % cnt ]).fadeIn("fast");
+				 
+			 });	 
+		 });
+		 
+		 $('#bildspel2').click(function() {
+			 
+					 $('#imageSlide').fadeOut("fast", function() {
+				
+         	  	 		 $(this).attr('src', imgs[(imgs.length++) % cnt ]).fadeIn("fast");
+        	 		 });	 
+		 });      
 });
+    
