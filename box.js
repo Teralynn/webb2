1bdcc3d8ad62751402246dee81aee1869be26192
Teralynn <daniel.hakansson.te11c@gmail.com>
2014-03-14 12:43:25 +0100

    Version 1.2
    Added real photoshop pictures and made it more fancy :)

--- box.js
+++ box.js
@@ -1,5 +1,5 @@
-// imgs är min array med bilder
-// cnt hjälper till räkna ut antal bilder i arrayen
+// imgs är min array med bilder, som skall fyllas när användaren lägger in bilder
+// antsl räknar ut antal bilder i arrayen
 // bildspel är knappen till vänster på sidan
 // bildspel2 är knappen till höger på sidan
 // Använder mig av (this).attr för att fadeIn rätt bild
@@ -8,16 +8,16 @@ $(document).ready(function(){
 var imgs = [								
            'photos/glad.jpg',
            'photos/mario.jpg',
-		   'photos/placeholder.jpg'
+		   'photos/placeholder.jpg',
+		   'photos/boom.jpg'
 		   ];
-var cnt = imgs.length;
-		
-		 
-		 $('#bildspel').click(function() {
-			 
+var antal = imgs.length;			
+	 
+		 $('#bildspel').click(function() {		
+				 
 			 $('#imageSlide').fadeOut("fast", function() {
 				 
-         	  	  $(this).attr('src', imgs[(imgs.length++) % cnt ]).fadeIn("fast");
+         	  	   $(this).attr('src', imgs[(imgs.length++) % antal ]).fadeIn("fast");
 				 
 			 });	 
 		 });
@@ -25,8 +25,8 @@ var cnt = imgs.length;
 		 $('#bildspel2').click(function() {
 			 
 					 $('#imageSlide').fadeOut("fast", function() {
-				
-         	  	 		 $(this).attr('src', imgs[(imgs.length++) % cnt ]).fadeIn("fast");
+						 
+				       	  	 $(this).attr('src', imgs[(imgs.length++) % antal ]).fadeIn("fast");
         	 		 });	 
 		 });      
 });
